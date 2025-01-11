30c69c8ee2dca502b4b689ecbbca1c4be141419e
scherenhaenden <scherenhaenden@gmail.com>
2025-01-11 18:03:46 +0100

    Add ModelManager module, components, service, and unit tests

---
+++ src/app/model-manager/model-config/model-config.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-config',
+  imports: [],
+  templateUrl: './model-config.component.html',
+  styleUrl: './model-config.component.css'
+})
+export class ModelConfigComponent {
+
+}
---
+++ src/app/model-manager/model-list/model-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-list',
+  imports: [],
+  templateUrl: './model-list.component.html',
+  styleUrl: './model-list.component.css'
+})
+export class ModelListComponent {
+
+}
